dbd393293dc0723aca1c8fdba6cd5b86bfc7e1cb
l2zh <dlwnsgml0109@naver.com>
2023-11-22 02:50:43 +0900

    [add] #10 article, 액티비티, xml, manifest 추가

--- app/src/main/java/com/example/longdroid/presentation/MainActivity.kt
+++ app/src/main/java/com/example/longdroid/presentation/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.longdroid.presentation
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.example.longdroid.R
+import com.example.longdroid.databinding.ActivityMainBinding
+import com.example.longdroid.util.binding.BindingActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/example/longdroid/presentation/article/ArticleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.longdroid.presentation.article
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.longdroid.R
+
+class ArticleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article)
+    }
+}
