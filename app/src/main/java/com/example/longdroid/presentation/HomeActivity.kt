6b4ca64123f96dfc3e7dd13d685eedb9732892a2
Lee yeon Jeen <jeendev2019@gmail.com>
2023-11-26 18:52:21 +0900

    [feat]#11 HomeActivity Event 버튼 구현

--- app/src/main/java/com/example/longdroid/presentation/HomeActivity.kt
+++ app/src/main/java/com/example/longdroid/presentation/HomeActivity.kt
@@ -1,13 +1,45 @@
 package com.example.longdroid.presentation
 
+
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.longdroid.R
+import com.example.longdroid.databinding.ActivityHomeBinding
+
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private val imageResources = arrayOf(R.drawable.img_home_event_green, R.drawable.img_home_event_red, R.drawable.img_home_event_blue)
+    private var currentImageIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(getLayoutInflater())
+        val view: View = binding.root
+        setContentView(view)
+
+        val imageView: ImageView = binding.ivEventList
+        val nextButton: ImageButton = binding.btnNext
+        val beforeButton: ImageButton = binding.btnBefore
+
+        nextButton.setOnClickListener {
+            currentImageIndex = (currentImageIndex + 1) % imageResources.size
+            imageView.setImageResource(imageResources[currentImageIndex])
+        }
+
+        beforeButton.setOnClickListener {
+            currentImageIndex = if (currentImageIndex > 0) {
+                currentImageIndex - 1
+            } else {
+                imageResources.size - 1
+            }
+            imageView.setImageResource(imageResources[currentImageIndex])
+        }
+
+
 
 
     }
