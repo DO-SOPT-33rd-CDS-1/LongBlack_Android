d26510f9950c464da8010fe4a36264a17e9a4cc1
m6z1 <sonmyungj1zz@gmail.com>
2023-11-29 00:26:40 +0900

    [feat] #22 서버 통신 안 됐을 시 토스트 메시지 출력

--- app/src/main/java/com/example/longdroid/presentation/note_list/viewholder/NoteListViewHolder.kt
+++ app/src/main/java/com/example/longdroid/presentation/note_list/viewholder/NoteListViewHolder.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.longdroid.data.di.HomeServicePool
 import com.example.longdroid.data.model.request.RequestLike
 import com.example.longdroid.databinding.ItemNoteBinding
+import com.example.longdroid.util.extension.setOnSingleClickListener
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -14,27 +15,42 @@ class NoteListViewHolder(binding: ItemNoteBinding) : RecyclerView.ViewHolder(bin
     private val btnLiked = binding.btnLiked
 
     init {
-        clickBtnLiked() // 초기화 블록에서 함수 호출
+        clickBtnLiked()
     }
 
     fun clickBtnLiked() {
-        btnLiked.setOnClickListener {
-            Toast.makeText(btnLiked.context, "Button clicked!", Toast.LENGTH_SHORT).show()
+        btnLiked.setOnSingleClickListener {
             CoroutineScope(Dispatchers.IO).launch {
                 try {
-                    val response = HomeServicePool.likedService.putLiked(RequestLike(true, 2))
+                    val response =
+                        HomeServicePool.likedService.putLiked(RequestLike(true, 2)) // /현재는 하드코딩
                     if (response.isSuccessful) {
                         val responseBody = response.body().toString()
                         Log.d("tongsin", responseBody)
                         Log.d("tongsin", response.code().toString())
                     } else {
                         Log.d("tongsin", "error")
+                        Toast.makeText(
+                            btnLiked.context,
+                            TOAST_MESSAGE,
+                            Toast.LENGTH_SHORT,
+                        )
+                            .show()
                     }
                 } catch (e: Exception) {
-                    // 예외 처리
                     Log.d("tongsin", e.toString())
+                    Toast.makeText(
+                        btnLiked.context,
+                        TOAST_MESSAGE,
+                        Toast.LENGTH_SHORT,
+                    )
+                        .show()
                 }
             }
         }
+    }
+
+    companion object {
+        const val TOAST_MESSAGE = "예기치 못한 오류로 인하여 좋아요 수정이 되지 않았어요"
     }
 }
