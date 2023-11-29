dd76c66414346e1c0f080d80530e5058c6388037
m6z1 <sonmyungj1zz@gmail.com>
2023-11-29 14:03:30 +0900

    [refactor] #22 토스트 메시지 출력하는 부분 확장함수 사용

--- app/src/main/java/com/example/longdroid/presentation/note_list/viewholder/NoteListViewHolder.kt
+++ app/src/main/java/com/example/longdroid/presentation/note_list/viewholder/NoteListViewHolder.kt
@@ -1,12 +1,12 @@
 package com.example.longdroid.presentation.note_list.viewholder
 
 import android.util.Log
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.longdroid.data.di.HomeServicePool
 import com.example.longdroid.data.model.request.RequestLike
 import com.example.longdroid.databinding.ItemNoteBinding
 import com.example.longdroid.util.extension.setOnSingleClickListener
+import com.example.longdroid.util.extension.showToast
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -22,29 +22,27 @@ class NoteListViewHolder(binding: ItemNoteBinding) : RecyclerView.ViewHolder(bin
         btnLiked.setOnSingleClickListener {
             CoroutineScope(Dispatchers.IO).launch {
                 try {
-                    val response =
-                        HomeServicePool.likedService.putLiked(RequestLike(true, 2)) // /현재는 하드코딩
+                    val response = HomeServicePool.likedService.putLiked(
+                        RequestLike(
+                            LIST_VIEW_CHECK,
+                            2,
+                        ),
+                    ) // postId 현재는 하드코딩
                     if (response.isSuccessful) {
                         val responseBody = response.body().toString()
                         Log.d("tongsin", responseBody)
                         Log.d("tongsin", response.code().toString())
                     } else {
                         Log.d("tongsin", "error")
-                        Toast.makeText(
-                            btnLiked.context,
+                        btnLiked.context.showToast(
                             TOAST_MESSAGE,
-                            Toast.LENGTH_SHORT,
                         )
-                            .show()
                     }
                 } catch (e: Exception) {
                     Log.d("tongsin", e.toString())
-                    Toast.makeText(
-                        btnLiked.context,
+                    btnLiked.context.showToast(
                         TOAST_MESSAGE,
-                        Toast.LENGTH_SHORT,
                     )
-                        .show()
                 }
             }
         }
@@ -52,5 +50,6 @@ class NoteListViewHolder(binding: ItemNoteBinding) : RecyclerView.ViewHolder(bin
 
     companion object {
         const val TOAST_MESSAGE = "예기치 못한 오류로 인하여 좋아요 수정이 되지 않았어요"
+        const val LIST_VIEW_CHECK = true
     }
 }
